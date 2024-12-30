e65af7d0eb11ed3d18e46f50f2e5e8205eb88e65
hamzafarooqarif <m.hamza.arif786@gmail.com>
2024-12-31 00:21:55 +0100

    fetched data in table

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,2 @@
+export const GOOGLE_SHEETS_ID = "1qS7yrrPvKIIJ_OvuYEyw1dNYq2nWCLU2k-IAIc4hTyU";
+export const GOOGLE_SHEETS_URL = `https://docs.google.com/spreadsheets/d/${GOOGLE_SHEETS_ID}/export?format=csv`;
---
+++ src/app/services/google-sheet-service/google-sheet-service.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GOOGLE_SHEETS_URL } from '../../constants/constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleSheetServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  getSheetData(): Observable<string> {
+    const csvUrl = GOOGLE_SHEETS_URL;
+    return this.http.get(csvUrl, { responseType: 'text' });
+  }
+}
