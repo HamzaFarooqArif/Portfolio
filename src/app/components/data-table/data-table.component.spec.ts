896db9bdbb5b32f495d128205da551ffe0a029af
hamzafarooqarif <m.hamza.arif786@gmail.com>
2024-12-31 05:15:23 +0100

    fixed test-cases

--- src/app/components/data-table/data-table.component.spec.ts
+++ src/app/components/data-table/data-table.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataTableComponent } from './data-table.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DataTableComponent', () => {
   let component: DataTableComponent;
@@ -8,7 +13,8 @@ describe('DataTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DataTableComponent]
+      declarations: [DataTableComponent],
+      imports: [HttpClientModule, MatFormFieldModule, MatSelectModule, ReactiveFormsModule, BrowserAnimationsModule],
     })
     .compileComponents();
     
--- src/app/services/spreadsheet/spreadsheet.service.spec.ts
+++ src/app/services/spreadsheet/spreadsheet.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { SpreadsheetService } from './spreadsheet.service';
 
 describe('SpreadsheetService', () => {
   let service: SpreadsheetService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [SpreadsheetService],
+    });
     service = TestBed.inject(SpreadsheetService);
   });
 
