18d91bb2c1f8185b74738bf46d19d5bf4ea6862a
hamzafarooqarif <m.hamza.arif786@gmail.com>
2025-01-01 19:00:34 +0100

    fixed tests

--- src/app/components/data-table/data-table.component.spec.ts
+++ src/app/components/data-table/data-table.component.spec.ts
@@ -6,6 +6,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxLoadingModule } from 'ngx-loading';
 
 describe('DataTableComponent', () => {
   let component: DataTableComponent;
@@ -14,7 +22,33 @@ describe('DataTableComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DataTableComponent],
-      imports: [HttpClientModule, MatFormFieldModule, MatSelectModule, ReactiveFormsModule, BrowserAnimationsModule],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSliderModule,
+        MatCheckboxModule,
+        MatIconModule,
+        ToastrModule.forRoot(),
+        NgxLoadingModule.forRoot({}),
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            // Mock any parameters, queryParams, or data used in your component
+            params: of({ id: 1 }), // Example route parameter
+            queryParams: of({ key: 'value' }), // Example query parameter
+            snapshot: {
+              data: { example: 'data' } // Example snapshot data
+            }
+          }
+        }
+      ]
     })
     .compileComponents();
     
