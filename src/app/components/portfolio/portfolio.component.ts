6eed6d446a376660a2759bf50b88c8c5bb08474d
hamzafarooqarif <m.hamza.arif786@gmail.com>
2025-01-19 22:56:54 +0100

    added portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit{
+
+  profession: string = "";
+  professions: string[] = [" Developer", "n Engineer"];
+
+  ngOnInit(): void {
+      this.animateProfession();
+  }
+
+  async animateProfession() {
+    for(let i = 0; i < this.professions.length; i++) {
+      for(let j = 0; j < this.professions[i].length; j++) {
+        this.profession += this.professions[i][j];
+        await this.delay(150);
+      }
+      await this.delay(1000);
+      for(let j = 0; j < this.professions[i].length; j++) {
+        this.profession = this.profession.substring(0, this.profession.length - 1);
+        await this.delay(150);
+      }
+    }
+    this.animateProfession();
+  }
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
 }
--- src/app/read-aloud/read-aloud-routing.module.ts
+++ src/app/read-aloud/read-aloud-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataTableComponent } from './components/data-table/data-table.component';
+import { MatButtonModule } from '@angular/material/button';
 
 const routes: Routes = [
   { path: '', component: DataTableComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
   exports: [RouterModule]
 })
 export class ReadAloudRoutingModule { }
