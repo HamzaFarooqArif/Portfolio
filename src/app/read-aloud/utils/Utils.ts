8c72df6f527b41b5bfc58e4d12ab77ecef30b1b9
hamzafarooqarif <m.hamza.arif786@gmail.com>
2025-01-21 02:40:18 +0100

    fix3

--- src/app/read-aloud/utils/Utils.ts
+++ src/app/read-aloud/utils/Utils.ts
@@ -1,4 +1,4 @@
-export class Utility {
+export class Utils {
     public static isEmpty = (obj: any): boolean => {
         return obj && typeof obj === 'object' && Object.keys(obj).length === 0;
     };
